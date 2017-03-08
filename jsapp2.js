86ed37f22e1dc5e3ec13e791d1b14c9e5e1702e6
warsztatcrp <kzaorski@pzu.pl>
2017-03-08 21:09:51 +0100

    Create jsapp2.js

---
+++ jsapp2.js
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link href="style.css" rel="stylesheet">
+    <title>Critical Path: Script External</title>
+  </head>
+  <body>
+    <p>Hello <span>web performance</span> students!</p>
+    <div><img src="awesome-photo.jpg"></div>
+    <script src="app2.js"></script>
+  </body>
+</html>
