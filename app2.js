24a2f9b5a8cf817aaf63fa0ce64647337f24fad5
warsztatcrp <kzaorski@pzu.pl>
2017-03-08 21:08:44 +0100

    Create app2.js

---
+++ app2.js
@@ -0,0 +1,9 @@
+var span = document.getElementsByTagName('span')[0];
+span.textContent = 'interactive'; // change DOM text content
+sleep(20)
+span.style.display = 'inline';  // change CSSOM property
+// create a new element, style it, and append it to the DOM
+var loadTime = document.createElement('div');
+loadTime.textContent = 'You loaded this page on: ' + new Date();
+loadTime.style.color = 'blue';
+document.body.appendChild(loadTime);
